dc99990f043d0c70a52734aa65ae9faf9b3ef47d
anasmohamed <manas9109@gmail.com>
2025-04-29 01:44:51 +0300

    create home view with mockk data

--- lib/presentation/views/home_view.dart
+++ lib/presentation/views/home_view.dart
@@ -1,10 +1,197 @@
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final List<Map<String, String>> items = [
+    {
+      'image': 'https://picsum.photos/200',
+      'title': 'Item 1',
+      'description': 'Description 1',
+    },
+    {
+      'image': 'https://picsum.photos/201',
+      'title': 'Item 2',
+      'description': 'Description 2',
+    },
+    {
+      'image': 'https://picsum.photos/202',
+      'title': 'Item 3',
+      'description': 'Description 3',
+    },
+    {
+      'image': 'https://picsum.photos/203',
+      'title': 'Item 4',
+      'description': 'Description 4',
+    },
+    {
+      'image': 'https://picsum.photos/200',
+      'title': 'Item 5',
+      'description': 'Description 1',
+    },
+    {
+      'image': 'https://picsum.photos/201',
+      'title': 'Item 6',
+      'description': 'Description 2',
+    },
+    {
+      'image': 'https://picsum.photos/202',
+      'title': 'Item 7',
+      'description': 'Description 3',
+    },
+    {
+      'image': 'https://picsum.photos/203',
+      'title': 'Item 8',
+      'description': 'Description 4',
+    },
+  ];
+
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
+  final TextEditingController imageUrlController = TextEditingController();
+
+  void showAddItemDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Add New Item'),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: titleController,
+                  decoration: InputDecoration(labelText: 'Title'),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  controller: descriptionController,
+                  decoration: InputDecoration(labelText: 'Description'),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  controller: imageUrlController,
+                  decoration: InputDecoration(labelText: 'Image URL'),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (titleController.text.isNotEmpty &&
+                    descriptionController.text.isNotEmpty &&
+                    imageUrlController.text.isNotEmpty) {
+                  setState(() {
+                    items.add({
+                      'title': titleController.text,
+                      'description': descriptionController.text,
+                      'image': imageUrlController.text,
+                    });
+                  });
+                  titleController.clear();
+                  descriptionController.clear();
+                  imageUrlController.clear();
+                  Navigator.pop(context);
+                }
+              },
+              child: Text('Save'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: GridView.builder(
+          itemCount: items.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            childAspectRatio: 3 / 4,
+          ),
+          itemBuilder: (context, index) {
+            final item = items[index];
+            return Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.shade300,
+                    blurRadius: 6,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius:
+                        const BorderRadius.vertical(top: Radius.circular(16)),
+                    child: Image.network(
+                      item['image']!,
+                      height: 120,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                      errorBuilder: (context, error, stackTrace) => Container(
+                        height: 120,
+                        color: Colors.grey[300],
+                        child: const Center(child: Icon(Icons.broken_image)),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      item['title']!,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      item['description']!,
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey[600],
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: showAddItemDialog,
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
